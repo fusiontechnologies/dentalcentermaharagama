c8699e6ec9d7358e878ec55fb04df5ef825fd837
Fusion Technologies <113576159+fusiontechnologies@users.noreply.github.com>
2022-09-20 17:07:09 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,3 +129,22 @@ jQuery(document).ready(function ($) {
 
 });
 
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("menu-active");
+    if (li.classList.contains(current)) {
+      li.classList.add("menu-active");
+    }
+  });
+};
